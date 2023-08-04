834c971d1356e9917918c1184f4db2cc9232d34b
devdada <you@example.com>
2023-08-03 23:56:43 -0500

    edit text on home page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome <code>To my</code> Portfolio
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Enter here
         </a>
       </header>
     </div>
