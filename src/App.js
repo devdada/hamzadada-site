89a5e64bc682d6e727d89b12001cf70b0f6e5ac4
devdada <you@example.com>
2023-08-03 23:57:34 -0500

    fix spacing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome <code>To my</code> Portfolio
+          Welcome<code>To my</code> Portfolio
         </p>
         <a
           className="App-link"
